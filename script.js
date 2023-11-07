563daa11d8770bbd6fc097b5b03ce01f02098acc
JustWacs <bagasm072@gmail.com>
2023-11-07 23:20:29 +0800

    Edited menu

--- script.js
+++ script.js
@@ -10,46 +10,43 @@ let charIndex = 0;
 
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
-  } 
-  else {
+  } else {
     cursorSpan.classList.remove("typing");
-  	setTimeout(erase, newTextDelay);
+    setTimeout(erase, newTextDelay);
   }
 }
 
 function erase() {
-	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
     charIndex--;
     setTimeout(erase, erasingDelay);
-  } 
-  else {
+  } else {
     cursorSpan.classList.remove("typing");
     textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
     setTimeout(type, typingDelay + 1100);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
+const poci = document.querySelector(".poci");
+const img = poci.querySelector("img");
 
-
-const poci = document.querySelector('.poci');
-const img = poci.querySelector('img');
-
-poci.addEventListener('mouseenter', () => {
-  gsap.to(img, { scale: 1.1, duration: 0.5, ease: 'power2.out' });
+poci.addEventListener("mouseenter", () => {
+  gsap.to(img, { scale: 1.1, duration: 0.5, ease: "power2.out" });
 });
 
-poci.addEventListener('mousemove', (e) => {
+poci.addEventListener("mousemove", (e) => {
   const containerRect = poci.getBoundingClientRect();
   const centerX = containerRect.left + containerRect.width / 2;
   const centerY = containerRect.top + containerRect.height / 2;
@@ -59,9 +56,64 @@ poci.addEventListener('mousemove', (e) => {
   const deltaX = (mouseX - centerX) / 15; // Sesuaikan faktor pergerakan
   const deltaY = (mouseY - centerY) / 15; // Sesuaikan faktor pergerakan
 
-  gsap.to(img, { x: deltaX, y: deltaY, duration: 0.2, ease: 'power2.out' });
+  gsap.to(img, { x: deltaX, y: deltaY, duration: 0.2, ease: "power2.out" });
 });
 
-poci.addEventListener('mouseleave', () => {
-  gsap.to(img, { scale: 1, x: 0, y: 0, duration: 0.5, ease: 'power2.out' });
+poci.addEventListener("mouseleave", () => {
+  gsap.to(img, { scale: 1, x: 0, y: 0, duration: 0.5, ease: "power2.out" });
 });
+
+// menu
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+  reset: false
+});
+
+ScrollReveal().reveal(".title", {
+  duration: 3000,
+  origin: "top",
+  distance: "400px",
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  rotate: {
+    x: 20,
+    z: -10
+  }
+});
+
+ScrollReveal().reveal(".fade-in", {
+  duration: 5000,
+  move: 0
+});
+
+ScrollReveal().reveal(".scaleUp", {
+  duration: 4000,
+  scale: 0.85
+});
+
+ScrollReveal().reveal(".flip", {
+  delay: 500,
+  duration: 2000,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+
+ScrollReveal().reveal(".slide-right", {
+  duration: 3000,
+  origin: "left",
+  distance: "300px",
+  easing: "ease-in-out"
+});
+
+ScrollReveal().reveal(".slide-up", {
+  duration: 500,
+  origin: "bottom",
+  distance: "50px",
+  easing: "cubic-bezier(.37,.01,.74,1)",
+  opacity: 0.3,
+  scale: 0.5
+});
+
+// menu end
