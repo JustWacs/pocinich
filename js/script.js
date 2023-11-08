ea6852705d2067095ca8fbd931c2af5744a5f49f
JustWacs <bagasm072@gmail.com>
2023-11-08 08:17:51 +0800

    Fix clashed javascript

---
+++ js/script.js
@@ -0,0 +1,131 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Panas?", "Haus?", "Poci aja"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+const poci = document.querySelector(".poci");
+const adit = poci.querySelector(".adit");
+
+poci.addEventListener("mouseenter", () => {
+  gsap.to(adit, { scale: 1.1, duration: 0.5, ease: "power2.out" });
+});
+
+poci.addEventListener("mousemove", (e) => {
+  const containerRect = poci.getBoundingClientRect();
+  const centerX = containerRect.left + containerRect.width / 2;
+  const centerY = containerRect.top + containerRect.height / 2;
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const deltaX = (mouseX - centerX) / 15; // Sesuaikan faktor pergerakan
+  const deltaY = (mouseY - centerY) / 15; // Sesuaikan faktor pergerakan
+
+  gsap.to(adit, { x: deltaX, y: deltaY, duration: 0.2, ease: "power2.out" });
+});
+
+poci.addEventListener("mouseleave", () => {
+  gsap.to(adit, { scale: 1, x: 0, y: 0, duration: 0.5, ease: "power2.out" });
+});
+
+// menu
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+  reset: false
+});
+
+ScrollReveal().reveal(".title", {
+  duration: 3000,
+  origin: "top",
+  distance: "400px",
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  rotate: {
+    x: 20,
+    z: -10
+  }
+});
+
+ScrollReveal().reveal(".fade-in", {
+  duration: 5000,
+  move: 0
+});
+
+ScrollReveal().reveal(".scaleUp", {
+  duration: 4000,
+  scale: 0.85
+});
+
+ScrollReveal().reveal(".flip", {
+  delay: 500,
+  duration: 2000,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+
+ScrollReveal().reveal(".slide-right", {
+  duration: 3000,
+  origin: "left",
+  distance: "300px",
+  easing: "ease-in-out"
+});
+
+ScrollReveal().reveal(".slide-up", {
+  duration: 500,
+  origin: "bottom",
+  distance: "50px",
+  easing: "cubic-bezier(.37,.01,.74,1)",
+  opacity: 0.3,
+  scale: 0.5
+});
+
+// menu end
+
+//Faq
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) =>{
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
+});
+
+//Faq end
